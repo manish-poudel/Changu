05c756df2aa0834c6c79e491a6102b8164fb1f45
Manish <omekus.co@gmail.com>
2020-07-15 15:04:57 +0545

    Added pinch gesture to scale donut (Dummy test object created in blender)

---
+++ Assets/MainGame/Scripts/AssetController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using GoogleARCore;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AssetController : MonoBehaviour
+{
+
+    private GameObject assetInstance;
+    private DetectedPlane detectedPlane;
+    public GameObject donutPrefab;
+    public bool spawn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartTracking();
+    }
+
+
+    /// Set selected plane. This method is called from Scene controller when user taps in the plane
+    public void SetSelectedPlane(DetectedPlane detectedPlane)
+    {
+        this.detectedPlane = detectedPlane;
+        if (spawn)
+        {
+            SpawnAsset();
+        }
+    }
+
+
+    /// Create anchor to place the asset fixed in the position despite camera movement
+    void SpawnAsset()
+    {
+        if (assetInstance != null)
+        {
+            DestroyImmediate(assetInstance);
+        }
+
+
+        Anchor anchor = detectedPlane.CreateAnchor(new Pose(detectedPlane.CenterPose.position, Quaternion.identity));
+        assetInstance = Instantiate(donutPrefab, detectedPlane.CenterPose.position, Quaternion.identity, anchor.transform);
+        assetInstance.transform.localScale = new Vector3(10f, 10f, 10f);
+        assetInstance.transform.SetParent(anchor.transform);
+    }
+
+    void StartTracking()
+    {
+        // If there is no plane, then return
+        if (detectedPlane == null)
+        {
+            return;
+        }
+
+        // Check for the plane being subsumed.
+        // If the plane has been subsumed switch attachment to the subsuming plane.
+        while (detectedPlane.SubsumedBy != null)
+        {
+            detectedPlane = detectedPlane.SubsumedBy;
+        }
+
+    }
+
+}
---
+++ Assets/MainGame/Scripts/GlobalScalable.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+/*
+Component class to allow the stretching and pinching to scale objects.
+*/
+public class GlobalScalable : MonoBehaviour
+{
+    private Vector3 originalScale;
+    static private float scaleFactor = 1.0f;
+    public bool handleScaleInput = false;
+    public bool adjustScale = true;
+    private float speed = 1f;
+
+    void Start()
+    {
+        /// Get object initial scale
+        originalScale = transform.localScale;
+    }
+
+
+    void Update()
+    {
+        handleInputTouch();
+    }
+
+    /// Handle input touch by the user.
+    void handleInputTouch()
+    {
+        /// Scale input is enabled
+        if (handleScaleInput)
+        {
+            /// If user is touching screen with two fingers
+            if (Input.touchCount == 2)
+            {
+                handleTwoInputTouch();
+            }
+        }
+    }
+
+
+    /// Handle if the input touch number is two.
+    void handleTwoInputTouch()
+    {
+        /// Store both touches
+        Touch touchZero = Input.GetTouch(0);
+        Touch touchOne = Input.GetTouch(1);
+
+        Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+        Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+        // Find the magnitude of the vector between the touches in current and previous frame
+        float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+        float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+        /// Find the difference in the distances
+        float deltaMagDiff = prevTouchDeltaMag - touchDeltaMag;
+
+        // range is 1 to 1000.
+        // The scale factor is v/100f meaning scale from 0.01 to 10.
+        float v = scaleFactor * 100f;
+
+        // negative delta is stretching, positive is pinching
+        v -= deltaMagDiff * speed;
+
+        scaleFactor = Mathf.Clamp(v, 1f, 1000f) / 100f;
+
+        /// Adjust scale
+        if(adjustScale)
+        {
+            transform.localScale = originalScale * scaleFactor;
+        }
+    }
+}
